f5768b72bdae1d95a00a6dbd27af250f81591062
Robin <rtmbaertschi007@gmail.com>
2025-01-26 19:53:55 +0100

    use branch instead of dedent

--- queries/indents.scm
+++ queries/indents.scm
@@ -2,8 +2,8 @@
  (block_expression)
  (grouped_expression)
  ] @indent.begin
-(block_expression "}" @indent.dedent)
-(grouped_expression ")" @indent.dedent)
+(block_expression "}" @indent.branch)
+(grouped_expression ")" @indent.branch)
 [
  "}"
  ")"
